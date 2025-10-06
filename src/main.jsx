5ddc83b4fd7060de0a4d652a957482783ad7aef5
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-10-07 01:30:18 +0600

    Understand The Concept of Routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,42 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './Components/Root/Root.jsx'
+import Home from './Components/Home/Home.jsx'
+import Mobiles from './Components/Mobiles/Mobiles.jsx'
+import Laptops from './Components/Laptops/Laptops.jsx'
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element: <div> Welcome to React router </div>
+    path: '/',
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: 'mobiles', Component: Mobiles },
+      { path: 'laptops', Component: Laptops }
+    ]
+  },
+  {
+    path: 'about',
+    element: <div>About me here</div>
+  },
+  {
+    path: 'blogs',
+    element: <div>All my blogs are here</div>
+  },
+  {
+    path: 'app',
+    Component: App
+  },
+  {
+    path: '/app2',
+    // element:<App></App>
+    Component: App
   }
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
-   
