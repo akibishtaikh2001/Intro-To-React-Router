8e75ff904e842bd4fed9b43132fdb1fcb95af68f
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-10-07 06:38:02 +0600

    Multiple ways to load data using loader and useLoaderData

---
+++ src/Components/Users/Users.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Users = () => {
+
+    const users = useLoaderData();
+    console.log(users);
+    
+
+    return (
+        <div>
+            <h2>This is Users page</h2>
+        </div>
+    );
+};
+
+export default Users;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,6 +7,10 @@ import Root from './Components/Root/Root.jsx'
 import Home from './Components/Home/Home.jsx'
 import Mobiles from './Components/Mobiles/Mobiles.jsx'
 import Laptops from './Components/Laptops/Laptops.jsx'
+import Users from './Components/Users/Users.jsx'
+import Users2 from './Components/Users2/Users2.jsx'
+
+const usersPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
 const router = createBrowserRouter([
   {
@@ -15,7 +19,18 @@ const router = createBrowserRouter([
     children: [
       { index: true, Component: Home },
       { path: 'mobiles', Component: Mobiles },
-      { path: 'laptops', Component: Laptops }
+      { path: 'laptops', Component: Laptops },
+      {
+        path: 'users',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
+        Component: Users
+      },
+      {
+        path: 'users2',
+        element: <Suspense fallback={ <span>Loading....</span> }>
+          <Users2 usersPromise={usersPromise} ></Users2>
+        </Suspense>
+      }
     ]
   },
   {
@@ -39,6 +54,9 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider
+      router={router}
+     
+    ></RouterProvider>
   </StrictMode>,
 )
